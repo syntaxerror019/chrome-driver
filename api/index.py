27b137e02b9d1da3c96dff11f32a38b1746126ae
syntaxerror <142847239+syntaxerror019@users.noreply.github.com>
2024-04-20 18:41:40 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    # URL of the JSON data
+    url = "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json"
 
-@app.route('/about')
-def about():
-    return 'About'
+    # Fetch the JSON data
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract values to populate the table
+    table_data = []
+    for version in data['versions']:
+        version_info = {
+            'version': version['version'],
+            'platforms': []
+        }
+        for download in version['downloads']['chrome']:
+            platform_info = {
+                'platform': download['platform'],
+                'url': download['url']
+            }
+            version_info['platforms'].append(platform_info)
+        table_data.append(version_info)
+
+    # Render the HTML template with the table data
+    return render_template('index.html', table_data=table_data)
